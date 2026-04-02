532339c767bda1f3a0e6ab645d14a2e39361829a
talzr <talzrawy30@gmail.com>
2026-04-02 16:09:56 +0100

    Fix code QR

---
+++ app/src/main/java/com/example/ai_guardian/QRScannerActivity.kt
@@ -0,0 +1,8 @@
+package com.example.ai_guardian
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import com.journeyapps.barcodescanner.CaptureActivity
+
+class QRScannerActivity : CaptureActivity()
